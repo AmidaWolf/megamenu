7b8ba0b9e573ab925ae3c3df53284168c0496830
Yauhen <amidawolf@gmail.com>
2021-08-25 22:21:58 +0300

    feat: add properties, small refactor

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# megamenu
-megamenu like https://rozetka.com.ua/ or https://allegro.pl/
+# Megamenu
+Megamenu like https://rozetka.com.ua/ or https://allegro.pl/ in pure js without libraries and module bandle (node, webpack and etc).
+Just stupid realization for simple example. 
+## Without mobile version (only for desktop)
--- megamenu.js
+++ megamenu.js
@@ -1,20 +1,33 @@
-const urlJSON = new URL('https://jsonplaceholder.typicode.com/posts'); //past url
+const urlJSON = new URL('http://localhost:3000/categories'); //past url
+const testUrl = './data.json';
 
-function createMenu (itemsList) {
+
+let megamenuProperties = {
+    testWithLocalData: true, //true - only for testing without url responses, use data.json
+    button: false, //true - add button-menu into id="megamenu_button_wrapper", dont work correctly
+    buttonText: "Categories",
+    parentPaddingWidth: 16, //padding-left 15px at col-3 + 1px border at card
+    imgForCategory: true, //true - add image into categories item (left side)
+    startIcon: false, //true - add icon into categories item (left side)
+    chevronIcon: "img", //"FA" - FontAwesome icons(<i>); "img" - <img> icons; none - without chevron icons (right side)
+    chevronIconUrl: "fas fa-angle-right" //past url or FontAwesome class name
+}
+
+let createMenu = (itemsList) => {
     let categoriesView = new DocumentFragment();
     let subcategoriesView = new DocumentFragment();
     let subsubcategoriesView = new DocumentFragment();
 
-    for(let i = 0; i<itemsList.length; i++) {
+    for (let i = 0; i < itemsList.length; i++) {
 
         if (itemsList[i].subcategories && itemsList[i].subcategories.length > 0) {
 
-            for(let x = 0; x<itemsList[i].subcategories.length; x++) {
+            for (let x = 0; x < itemsList[i].subcategories.length; x++) {
 
                 if (itemsList[i].subcategories[x].subsubcategories
                     && itemsList[i].subcategories[x].subsubcategories.length > 0) {
 
-                    for(let y = 0; y<itemsList[i].subcategories[x].subsubcategories.length; y++) {
+                    for (let y = 0; y < itemsList[i].subcategories[x].subsubcategories.length; y++) {
                         let subsubcategories = document.createElement("li");
                         subsubcategories.className = "subcategories__item";
 
@@ -57,43 +70,82 @@ function createMenu (itemsList) {
 
         }
 
-        let startIconLink = itemsList[i].startIcon;
-        let endIconLink = itemsList[i].endIcon;
-        let imageCategoryLink = itemsList[i].img;
+        let startIcon;
+
+        if (megamenuProperties.startIcon) {
+            let startIconLink = itemsList[i].startIcon;
+
+            startIcon = document.createElement("img");
+            startIcon.className = "categories__icon";
+            startIcon.src = startIconLink;
+            startIcon.alt = "icon";
+            startIcon.height = 24;
+            startIcon.width = 24;
+        }
+
+        let endIcon;
+
+        if (megamenuProperties.chevronIcon === "img") {
+            let endIconLink = itemsList[i].endIcon;
+            if (endIconLink === undefined) {
+                endIconLink = megamenuProperties.chevronIconUrl;
+                if (endIconLink.length === 0) {
+                    endIconLink = "https://via.placeholder.com/15x9";
+                }
+            }
+
+            endIcon = document.createElement("img");
+            endIcon.className = "categories__link-chevron";
+            endIcon.src = endIconLink;
+            endIcon.alt = "chevron";
+            endIcon.height = 15;
+            endIcon.width = 9;
+        }
+
+        if (megamenuProperties.chevronIcon === "FA") {
+            let endIconLink = megamenuProperties.chevronIconUrl;
+
+            endIcon = document.createElement("i");
+            endIcon.className = endIconLink;
+            endIcon.alt = "chevron";
+            endIcon.style.fontSize = "12px"
+            endIcon.style.marginLeft = "5px";  //maybe replace style into megamenu.css, inline so bad
+        }
+
         let link = itemsList[i].href;
         let linkText = document.createTextNode(itemsList[i].name);
 
-        let startIcon = document.createElement("img");
-        startIcon.className = "categories__icon";
-        startIcon.src = startIconLink;
-        startIcon.alt = "icon";
-        startIcon.height = 24;
-        startIcon.width = 24;
-
-        let endIcon = document.createElement("img");
-        endIcon.className = "categories__link-chevron";
-        endIcon.src = endIconLink;
-        endIcon.alt = "chevron";
-        endIcon.height = 15;
-        endIcon.width = 9;
-
-        let imageCategory = document.createElement("img");
-        imageCategory.className = "categories__img";
-        imageCategory.src = imageCategoryLink;
-        imageCategory.alt = itemsList[i].name;
-        imageCategory.height = 100;
-        imageCategory.width = 100;
-        imageCategory.style.display = "none";
-
+        let imageCategory;
+        if (megamenuProperties.imgForCategory) {
+            let imageCategoryLink = itemsList[i].img;
+            if (!imageCategoryLink || imageCategoryLink.length === 0) {
+                imageCategoryLink = "https://via.placeholder.com/100";
+            }
+            imageCategory = document.createElement("img");
+            imageCategory.className = "categories__img";
+            imageCategory.src = imageCategoryLink;
+            imageCategory.alt = itemsList[i].name;
+            imageCategory.height = 100;
+            imageCategory.width = 100;
+        }
 
         let a = document.createElement("a");
         a.className = "categories__link";
         a.tabIndex = 1;
-        a.appendChild(imageCategory);
-        a.appendChild(startIcon);
-        a.appendChild(linkText);
-        a.appendChild(endIcon);
         a.href = link;
+        if (megamenuProperties.imgForCategory) {
+            a.appendChild(imageCategory);
+        }
+        if (megamenuProperties.startIcon) {
+            a.appendChild(startIcon);
+        }
+        a.appendChild(linkText);
+        if (megamenuProperties.chevronIcon === "img") {
+            a.appendChild(endIcon);
+        }
+        if (megamenuProperties.chevronIcon === "FA") {
+            a.appendChild(endIcon);
+        }
 
         let li = document.createElement('li');
         li.className = "categories__item";
@@ -107,18 +159,50 @@ function createMenu (itemsList) {
         subcategoryWrapper.appendChild(subcategoriesView);
 
         categoriesView.append(li);
-        }
+    }
 
     let menuListItems = document.createElement('ul');
     menuListItems.className = "categories";
     menuListItems.append(categoriesView);
 
+    menuListItems.childNodes[0].childNodes[1].id = "sub_wrapper_first";
+
     return menuListItems;
 }
 
-function drawMenu (data) {
+let drawButton = () => {
+    const buttonWrapper = document.getElementById("megamenu_button_wrapper");
+    const subcategoryWrapperFirst = document.getElementById("sub_wrapper_first");
+    let button = document.createElement("button");
+    button.id = "megamenu_button";
+    button.className = "megamenu-button";
+    button.append(megamenuProperties.buttonText);
+
+    buttonWrapper.append(button);
+
+    button.addEventListener("click", function (e) {
+        if (e.target === button && getComputedStyle(subcategoryWrapperFirst).display !== "block") {
+            subcategoryWrapperFirst.classList.add("active");
+        } else if (e.target === button && getComputedStyle(subcategoryWrapperFirst).display === "block") {
+            subcategoryWrapperFirst.classList.remove("active");
+        }
+    });
+
+    subcategoryWrapperFirst.addEventListener("mouseleave", function (e) {
+        if (e.target === subcategoryWrapperFirst && getComputedStyle(subcategoryWrapperFirst).display === "block") {
+            subcategoryWrapperFirst.classList.remove("active");
+        }
+    });
+
+}
+
+let drawMenu = (data) => {
     const megamenu = document.getElementById("megamenu");
-    megamenu.append(createMenu(data.categories));
+    megamenu.append(createMenu(data));
+
+    if (megamenuProperties.button === true) {
+        drawButton();
+    }
 
     // calculate subcategory-wrapper width and height, del if width&height set in css
     const megamenuWidth = parseInt(window.getComputedStyle(megamenu).width);
@@ -129,7 +213,7 @@ function drawMenu (data) {
     const categoriesItems = document.getElementsByClassName("categories__item");
     const categoriesItemsHeight = parseInt(window.getComputedStyle(categoriesItems[0]).height);
     const wrapperWidth = (pageWidth - megamenuWidth) + "px";
-    const wrapperHeight = (categoriesItemsHeight * categoriesItems.length) +"px";
+    const wrapperHeight = (categoriesItemsHeight * categoriesItems.length) + 5 + "px";
 
     for (let i = 0; i < wrapper.length; i++) {
         wrapper[i].style.width = wrapperWidth;
@@ -137,22 +221,23 @@ function drawMenu (data) {
     }
 }
 
-function initializeMenu (url) {
+const initializeMenu = (url) => {
     let xhr = new XMLHttpRequest();
     xhr.open('GET', url);
     xhr.send();
 
-    xhr.onload = function() {
+    xhr.onload = () => {
         if (xhr.status !== 200 && xhr.readyState !== 4) {
             console.log(`Ошибка ${xhr.status}: ${xhr.statusText}`);
         } else {
             console.log(`Готово, получили ${xhr.response.length} байт`);
-            let data = xhr.response;
-            drawMenu(data);
+            let data = JSON.parse(xhr.response);
+            drawMenu(data.categories);  //change this, if ur json have another enter point
+
         }
     };
 
-    xhr.onprogress = function(event) {
+    xhr.onprogress = (event) => {
         if (event.lengthComputable) {
             console.log(`Получено ${event.loaded} из ${event.total} байт`);
         } else {
@@ -161,14 +246,13 @@ function initializeMenu (url) {
 
     };
 
-    xhr.onerror = function() {
+    xhr.onerror = () => {
         console.log("Запрос не удался");
     };
 }
 
-// drawMenu(dataJSON);
-
-initializeMenu(urlJSON);
+let activeUrl = megamenuProperties.testWithLocalData ? testUrl : urlJSON;
+initializeMenu(activeUrl);
 
 
 
